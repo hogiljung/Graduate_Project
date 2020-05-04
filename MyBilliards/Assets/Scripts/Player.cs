0dad83f68e64833010886c5eba4d319839179c50
Jun <rara1367@naver.com>
2020-05-05 06:57:54 +0900

    플레이어 테이블 침범 막음 버튼이동 패드 좌표값으로 자연스럽게 되도록 수정

---
+++ MyBilliards/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public BoxCollider boxCollider;
+    public GameObject playerCam;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
