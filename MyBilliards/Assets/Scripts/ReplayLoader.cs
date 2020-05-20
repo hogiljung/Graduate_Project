5727a0201d841a07aaaf6d5ce5b2437b9fe38275
Jun <rara1367@naver.com>
2020-05-20 13:16:42 +0900

    오디오 믹서 볼륨조절 적용

---
+++ MyBilliards/Assets/Scripts/ReplayLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplayLoader : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Init()
+    {
+        int yValue = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            //DB에서 리플레이 불러오는 코드
+        }
+    }
+}
--- MyBilliards/Assets/Scripts/SoundSlider.cs
+++ MyBilliards/Assets/Scripts/SoundSlider.cs
@@ -1,21 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Audio;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SoundSlider : MonoBehaviour
 {
-    public AudioMixerEffectPlugin bgm;
+    public AudioMixer mixer;
     public Slider slider;
-    
+
+    private void Start()
+    {
+        
+    }
+
+    public void MaterControl()
+    {
+        float sound = slider.value;
+
+        if (sound == -40f)
+            mixer.SetFloat("Master", -80f);
+        else
+            mixer.SetFloat("Master", sound);
+    }
+
     public void BGMControl()
     {
         float sound = slider.value;
 
         if (sound == -40f)
-            bgm.SetFloatParameter("BGM", -80f);
+            mixer.SetFloat("BGM", -80f);
         else
-            bgm.SetFloatParameter("BGM", sound);
+            mixer.SetFloat("BGM", sound);
+    }
+
+    public void SFXComtrol()
+    {
+        float sound = slider.value;
+
+        if (sound == -40f)
+            mixer.SetFloat("SFX", -80f);
+        else
+            mixer.SetFloat("SFX", sound);
+    }
+
+    public void UIControl()
+    {
+        float sound = slider.value;
+
+        if (sound == -40f)
+            mixer.SetFloat("UI", -80f);
+        else
+            mixer.SetFloat("UI", sound);
     }
 }
